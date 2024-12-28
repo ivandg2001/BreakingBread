6aa3a1562c6decb6d07ed9ac80cefd89caa1cdeb
allembli41 <alessandrocastellano4141@gmail.com>
2024-12-28 13:08:45 +0100

    popolazione classe Sostanza e modifica documentazione classe Lotto

--- src/main/java/PackageArmadietto/Lotto.java
+++ src/main/java/PackageArmadietto/Lotto.java
@@ -17,13 +17,12 @@ public class Lotto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ID;
-
     /**
      * Data di scadenza del lotto
      */
     private Date data_scadenza;
     /**
-     * quantità di sostanza attualmente presente nel lotto
+     * Quantità di sostanza attualmente presente nel lotto
      */
     private double quantita;
 
@@ -32,11 +31,31 @@ public class Lotto {
      */
     public Lotto() {}
 
-    public Date getData_scadenza() {return data_scadenza;}
+    /**
+     * Restituisce la data di scadenza del lotto
+     *
+     * @return data di scadenza dell'oggetto corrente
+     */
+    public Date getData_scadenza() {return this.data_scadenza;}
 
+    /**
+     * Imposta la data di scadenza di un lotto
+     *
+     * @param data_scadenza data di scadenza
+     */
     public void setData_scadenza(Date data_scadenza) {this.data_scadenza = data_scadenza;}
 
+    /**
+     * Restituisce la quantità di sostanza presente nel lotto
+     *
+     * @return quantità della sostanza nel lotto
+     */
     public double getQuantita() {return quantita;}
 
+    /**
+     * Imposta la data quantità di sostanza nel lotto
+     *
+     * @param quantita quantità di sostanza nel lotto
+     */
     public void setQuantita(double quantita) {this.quantita = quantita;}
 }
--- src/main/java/PackageArmadietto/Sostanza.java
+++ src/main/java/PackageArmadietto/Sostanza.java
@@ -1,4 +1,89 @@
 package PackageArmadietto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
 public class Sostanza {
+
+    /**
+     * ID usato per identificare singolarmente le sostanze nel database
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+    /**
+     * Data di scadenza del lotto
+     */
+    private String nome;
+    /**
+     * Quantità di sostanza attualmente presente nel lotto
+     */
+    private String formula;
+    /**
+     * Costo della sostanza per ogni unità della stessa
+     */
+    private double costo_unitario;
+
+    /**
+     * Costruttore vuoto
+     */
+    public Sostanza() {}
+
+
+    /**
+     * Restituisce il nome della sostanza
+     *
+     * @return nome della sostanza
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Imposta il nome della sostanza
+     *
+     * @param nome nome della sostanza
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Restituisce la formula della sostanza
+     *
+     * @return formula della sostanza
+     */
+    public String getFormula() {
+        return formula;
+    }
+
+    /**
+     * Imposta la formula della sostanza
+     *
+     * @param formula formula della sostanza
+     */
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+
+    /**
+     * Restituisce il costo della sostanza per unità
+     *
+     * @return costo della sostanza per unità
+     */
+    public double getCosto_unitario() {
+        return costo_unitario;
+    }
+
+    /**
+     * Imposta il costo della sostanza per unitò
+     *
+     * @param costo_unitario costo della sostanza per unità
+     */
+    public void setCosto_unitario(double costo_unitario) {
+        this.costo_unitario = costo_unitario;
+    }
 }
