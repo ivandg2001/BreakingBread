df3262f8c229b9831f201fccbdc729331170ac15
allembli41 <alessandrocastellano4141@gmail.com>
2024-12-28 11:31:55 +0100

    aggiunta classe Lotto

--- src/main/java/PackageArmadietto/Lotto.java
+++ src/main/java/PackageArmadietto/Lotto.java
@@ -1,4 +1,24 @@
 package PackageArmadietto;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Lotto {
+
+    @Id
+    @GeneratedValue
+    private int ID;
+
+    private Date data_scadenza;
+    private double quantity;
+
+    public Date getData_scadenza() {return data_scadenza;}
+
+    public void setData_scadenza(Date data_scadenza) {this.data_scadenza = data_scadenza;}
+
+    public double getQuantity() {return quantity;}
+
+    public void setQuantity(double quantity) {this.quantity = quantity;}
 }
