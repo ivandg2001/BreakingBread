167f27f452bb8c94f70df1ec22a2ca84344ee113
allembli41 <alessandrocastellano4141@gmail.com>
2024-12-30 12:42:12 +0100

    modifiche a NuovoOrdineController e ArmadiettoFacade

--- src/main/java/PackageResponsabile/NuovoOrdineController.java
+++ src/main/java/PackageResponsabile/NuovoOrdineController.java
@@ -1,4 +1,37 @@
 package PackageResponsabile;
 
+import PackageArmadietto.Armadietto;
+import PackageArmadietto.ArmadiettoFacade;
+import PackageArmadietto.Lotto;
+import PackageArmadietto.Sostanza;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class NuovoOrdineController {
+
+    public static void creaNuovoOrdine(Responsabile responsabile) {
+
+        // acquisizione lista sostanze
+        ArmadiettoFacade armadiettoFacade = new ArmadiettoFacade();
+        ArrayList<Sostanza> listaSostanze = armadiettoFacade.getListaSostanze();
+
+        // crea form da compilare
+
+        //info dal form
+
+
+        // creazione dell'oggetto ordine
+        Lotto lotto = new Lotto(temp);
+        LocalDate today = LocalDate.now();
+        Ordine ordine = new Ordine(today, 12.3, responsabile, lotto);
+
+        // crea schermata di riepilogo
+        String nomeSostanza = armadiettoFacade.getNomeSostanza();
+        String formulaChimica = armadiettoFacade.getFormulaChimicaSostanza();
+        Double purezza = armadiettoFacade.getPurezzaSostanza();
+
+        // Inserimento dell'ordine nel sistema
+        //DAO
+    }
 }
--- src/main/java/PackageRicercatore/PrelievoSostanzaController.java
+++ src/main/java/PackageRicercatore/PrelievoSostanzaController.java
@@ -1,4 +1,21 @@
 package PackageRicercatore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrelievoSostanzaController {
+
+    /**
+     * Implementazione della funzionalità di prelievo di una sostanza
+     */
+    public static void prelevaSostanza(Ricercatore ricercatore) {
+
+        // acquisire lista di team e porgetti
+        ArrayList<Team> teams = Team.getTeams();
+        ArrayList<Progetto> progetti = Progetto.getProgetti();
+
+        // creazione dell'interfaccia grafica dell'utente
+
+
+    }
 }
